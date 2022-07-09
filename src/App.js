d36f292c94e777d640926f56b61894cd37706705
Abhisek Paul <abhisekpaul@Abhiseks-MacBook-Air.local>
2022-07-09 23:18:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import { Button, Typography } from '@cred/neopop-web/lib/components'
+import { FontVariant } from '@cred/neopop-web/lib/primitives'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <Typography {...FontVariant.HeadingBold20} fontSize='32' style={{margin: '2rem 0'}} >
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        </Typography>
+        <Button
+          variant='primary'
+          kind='elevated'
+          size='big'
+          colorMode='dark'
+          onClick={() => {
+            console.log("I'm clicked")
+          }}
         >
           Learn React
-        </a>
+        </Button>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
